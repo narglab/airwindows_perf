d34cde64f8d5175eb1be57cfa523c9d847498785
gaetan <g@nardsp.com>
2026-04-08 19:24:50 +0200

    Improve Acceleration

--- plugins/WinVST/Acceleration/AccelerationProc.cpp
+++ plugins/WinVST/Acceleration/AccelerationProc.cpp
@@ -1,27 +1,28 @@
-/* ========================================
- *  Acceleration - Acceleration.h
+/*
+ *  Acceleration - AccelerationProc.cpp
  *  Copyright (c) 2016 airwindows, Airwindows uses the MIT license
- * ======================================== */
+ *  Copyright (c) 2026 narglab
+*/
 
 #ifndef __Acceleration_H
 #include "Acceleration.h"
 #endif
 
-void Acceleration::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) 
+void Acceleration::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames)
 {
-    float* in1  =  inputs[0];
-    float* in2  =  inputs[1];
-    float* out1 = outputs[0];
-    float* out2 = outputs[1];
+	float *in1 = inputs[0];
+	float *in2 = inputs[1];
+	float *out1 = outputs[0];
+	float *out2 = outputs[1];
 
 	double overallscale = 1.0;
-	overallscale /= 44100.0;
+	overallscale *= inv_reference_samplerate;
 	overallscale *= getSampleRate();
-	
-	double intensity = pow(A,3)*(32/overallscale);
+
+	const double intensity = A * A * A * (32 / overallscale);
+	const double intensity_sq = intensity * intensity;
 	double wet = B;
-	//removed extra dry variable
-	
+
 	double senseL;
 	double smoothL;
 	double senseR;
@@ -31,105 +32,116 @@ void Acceleration::processReplacing(float **inputs, float **outputs, VstInt32 sa
 	double drySampleR;
 	double inputSampleL;
 	double inputSampleR;
-	    
-    while (--sampleFrames >= 0)
-    {
+
+	while (--sampleFrames >= 0)
+	{
 		inputSampleL = *in1;
 		inputSampleR = *in2;
-		if (fabs(inputSampleL)<1.18e-23) inputSampleL = fpdL * 1.18e-17;
-		if (fabs(inputSampleR)<1.18e-23) inputSampleR = fpdR * 1.18e-17;
+		if (std::abs(inputSampleL) < 1.18e-23) inputSampleL = fpdL * 1.18e-17;
+		if (std::abs(inputSampleR) < 1.18e-23) inputSampleR = fpdR * 1.18e-17;
 		drySampleL = inputSampleL;
 		drySampleR = inputSampleR;
-		
+
 		s3L = s2L;
 		s2L = s1L;
 		s1L = inputSampleL;
-		smoothL = (s3L + s2L + s1L) / 3.0;
-		m1L = (s1L-s2L)*((s1L-s2L)/1.3);
-		m2L = (s2L-s3L)*((s1L-s2L)/1.3);
-		senseL = fabs(m1L-m2L);
-		senseL = (intensity*intensity*senseL);
+		smoothL = (s3L + s2L + s1L) * inv_3;
+		const double diff_s1L_s2L = s1L - s2L;
+		m1L = diff_s1L_s2L * (diff_s1L_s2L * inv_one_point_three);
+		m2L = (s2L - s3L) * (diff_s1L_s2L * inv_one_point_three);
+		senseL = std::abs(m1L - m2L);
+		senseL = intensity_sq * senseL;
 		o3L = o2L;
 		o2L = o1L;
 		o1L = senseL;
-		if (o2L > senseL) senseL = o2L;
-		if (o3L > senseL) senseL = o3L;
+		senseL = o2L > senseL ? o2L : senseL;
+		senseL = o3L > senseL ? o3L : senseL;
 		//sense on the most intense
-		
+
 		s3R = s2R;
 		s2R = s1R;
 		s1R = inputSampleR;
-		smoothR = (s3R + s2R + s1R) / 3.0;
-		m1R = (s1R-s2R)*((s1R-s2R)/1.3);
-		m2R = (s2R-s3R)*((s1R-s2R)/1.3);
-		senseR = fabs(m1R-m2R);
-		senseR = (intensity*intensity*senseR);
+		smoothR = (s3R + s2R + s1R) * inv_3;
+		const double diff_s1R_s2R = s1R - s2R;
+		m1R = diff_s1R_s2R * (diff_s1R_s2R * inv_one_point_three);
+		m2R = (s2R - s3R) * (diff_s1R_s2R * inv_one_point_three);
+		senseR = std::abs(m1R - m2R);
+		senseR = intensity_sq * senseR;
 		o3R = o2R;
 		o2R = o1R;
 		o1R = senseR;
-		if (o2R > senseR) senseR = o2R;
-		if (o3R > senseR) senseR = o3R;
+		senseR = o2R > senseR ? o2R : senseR;
+		senseR = o3R > senseR ? o3R : senseR;
 		//sense on the most intense
-		
-		if (senseL > 1.0) senseL = 1.0;
-		if (senseR > 1.0) senseR = 1.0;
-		
-		inputSampleL *= (1.0-senseL);
-		inputSampleR *= (1.0-senseR);
-		
-		inputSampleL += (smoothL*senseL);
-		inputSampleR += (smoothR*senseR);
-		
-		senseL /= 2.0;
-		senseR /= 2.0;
-		
-		accumulatorSample = (ataLastOutL*senseL)+(inputSampleL*(1.0-senseL));
+
+		senseL = senseL > 1.0 ? 1.0 : senseL;
+		senseR = senseR > 1.0 ? 1.0 : senseR;
+
+		inputSampleL *= (1.0 - senseL);
+		inputSampleR *= (1.0 - senseR);
+
+		inputSampleL += (smoothL * senseL);
+		inputSampleR += (smoothR * senseR);
+
+		senseL *= 0.5;
+		senseR *= 0.5;
+
+		accumulatorSample = (ataLastOutL * senseL) + (inputSampleL * (1.0 - senseL));
 		ataLastOutL = inputSampleL;
 		inputSampleL = accumulatorSample;
-		
-		accumulatorSample = (ataLastOutR*senseR)+(inputSampleR*(1.0-senseR));
+
+		accumulatorSample = (ataLastOutR * senseR) + (inputSampleR * (1.0 - senseR));
 		ataLastOutR = inputSampleR;
-		inputSampleR = accumulatorSample;		
-		
-		if (wet !=1.0) {
-			inputSampleL = (inputSampleL * wet) + (drySampleL * (1.0-wet));
-			inputSampleR = (inputSampleR * wet) + (drySampleR * (1.0-wet));
+		inputSampleR = accumulatorSample;
+
+		if (wet != 1.0)
+		{
+			const double om_wet = 1.0 - wet;
+			inputSampleL = (inputSampleL * wet) + (drySampleL * om_wet);
+			inputSampleR = (inputSampleR * wet) + (drySampleR * om_wet);
 		}
-		
+
 		//begin 32 bit stereo floating point dither
-		int expon; frexpf((float)inputSampleL, &expon);
+		int expon;
+		constexpr uint32_t magic_number_dither = uint32_t(0x7fffffff);
+		constexpr uint64_t two_pow_62 = 1ULL << 62;
+		std::frexp((float) inputSampleL, &expon);
 		fpdL ^= fpdL << 13; fpdL ^= fpdL >> 17; fpdL ^= fpdL << 5;
-		inputSampleL += ((double(fpdL)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
-		frexpf((float)inputSampleR, &expon);
+		uint64_t power_of_twoL = two_pow_62 << expon;
+		double dither_factorL = 5.5e-36l * power_of_twoL;
+		inputSampleL += ((double(fpdL) - magic_number_dither) * dither_factorL);
+		std::frexp((float) inputSampleR, &expon);
 		fpdR ^= fpdR << 13; fpdR ^= fpdR >> 17; fpdR ^= fpdR << 5;
-		inputSampleR += ((double(fpdR)-uint32_t(0x7fffffff)) * 5.5e-36l * pow(2,expon+62));
+		uint64_t power_of_twoR = two_pow_62 << expon;
+		double dither_factorR = 5.5e-36l * power_of_twoR;
+		inputSampleR += ((double(fpdR) - magic_number_dither) * dither_factorR);
 		//end 32 bit stereo floating point dither
 
-		*out1 = inputSampleL;
-		*out2 = inputSampleR;
+		*out1 = static_cast<float>(inputSampleL);
+		*out2 = static_cast<float>(inputSampleR);
 
 		*in1++;
 		*in2++;
 		*out1++;
 		*out2++;
-    }
+	}
 }
 
-void Acceleration::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames) 
+void Acceleration::processDoubleReplacing(double **inputs, double **outputs, VstInt32 sampleFrames)
 {
-    double* in1  =  inputs[0];
-    double* in2  =  inputs[1];
-    double* out1 = outputs[0];
-    double* out2 = outputs[1];
+	double *in1 = inputs[0];
+	double *in2 = inputs[1];
+	double *out1 = outputs[0];
+	double *out2 = outputs[1];
 
 	double overallscale = 1.0;
-	overallscale /= 44100.0;
+	overallscale *= inv_reference_samplerate;
 	overallscale *= getSampleRate();
-	
-	double intensity = pow(A,3)*(32/overallscale);
-	double wet = B;
-	//removed extra dry variable
-	
+
+	const double intensity = A * A * A * (32 / overallscale);
+	const double intensity_sq = intensity * intensity;
+	const double wet = B;
+
 	double senseL;
 	double smoothL;
 	double senseR;
@@ -139,80 +151,78 @@ void Acceleration::processDoubleReplacing(double **inputs, double **outputs, Vst
 	double drySampleR;
 	double inputSampleL;
 	double inputSampleR;
-	
-    while (--sampleFrames >= 0)
-    {
+
+	while (--sampleFrames >= 0)
+	{
 		inputSampleL = *in1;
 		inputSampleR = *in2;
-		if (fabs(inputSampleL)<1.18e-23) inputSampleL = fpdL * 1.18e-17;
-		if (fabs(inputSampleR)<1.18e-23) inputSampleR = fpdR * 1.18e-17;
+		if (std::abs(inputSampleL) < 1.18e-23) inputSampleL = fpdL * 1.18e-17;
+		if (std::abs(inputSampleR) < 1.18e-23) inputSampleR = fpdR * 1.18e-17;
 		drySampleL = inputSampleL;
 		drySampleR = inputSampleR;
-		
+
 		s3L = s2L;
 		s2L = s1L;
 		s1L = inputSampleL;
-		smoothL = (s3L + s2L + s1L) / 3.0;
-		m1L = (s1L-s2L)*((s1L-s2L)/1.3);
-		m2L = (s2L-s3L)*((s1L-s2L)/1.3);
-		senseL = fabs(m1L-m2L);
-		senseL = (intensity*intensity*senseL);
+		smoothL = (s3L + s2L + s1L) * inv_3;
+		const double diff_s1L_s2L = s1L - s2L;
+		m1L = diff_s1L_s2L * (diff_s1L_s2L * inv_one_point_three);
+		m2L = (s2L - s3L) * (diff_s1L_s2L * inv_one_point_three);
+		senseL = std::abs(m1L - m2L);
+		senseL = intensity_sq * senseL;
 		o3L = o2L;
 		o2L = o1L;
 		o1L = senseL;
-		if (o2L > senseL) senseL = o2L;
-		if (o3L > senseL) senseL = o3L;
+		senseL = o2L > senseL ? o2L : senseL;
+		senseL = o3L > senseL ? o3L : senseL;
 		//sense on the most intense
-		
+
 		s3R = s2R;
 		s2R = s1R;
 		s1R = inputSampleR;
-		smoothR = (s3R + s2R + s1R) / 3.0;
-		m1R = (s1R-s2R)*((s1R-s2R)/1.3);
-		m2R = (s2R-s3R)*((s1R-s2R)/1.3);
-		senseR = fabs(m1R-m2R);
-		senseR = (intensity*intensity*senseR);
+		smoothR = (s3R + s2R + s1R) * inv_3;
+		const double diff_s1R_s2R = s1R - s2R;
+		m1R = diff_s1R_s2R * (diff_s1R_s2R * inv_one_point_three);
+		m2R = (s2R - s3R) * (diff_s1R_s2R * inv_one_point_three);
+		senseR = std::abs(m1R - m2R);
+		senseR = intensity_sq * senseR;
 		o3R = o2R;
 		o2R = o1R;
 		o1R = senseR;
-		if (o2R > senseR) senseR = o2R;
-		if (o3R > senseR) senseR = o3R;
+		senseR = o2R > senseR ? o2R : senseR;
+		senseR = o3R > senseR ? o3R : senseR;
 		//sense on the most intense
-		
-		if (senseL > 1.0) senseL = 1.0;
-		if (senseR > 1.0) senseR = 1.0;
-		
-		inputSampleL *= (1.0-senseL);
-		inputSampleR *= (1.0-senseR);
-		
-		inputSampleL += (smoothL*senseL);
-		inputSampleR += (smoothR*senseR);
-		
-		senseL /= 2.0;
-		senseR /= 2.0;
-		
-		accumulatorSample = (ataLastOutL*senseL)+(inputSampleL*(1.0-senseL));
+
+		senseL = senseL > 1.0 ? 1.0 : senseL;
+		senseR = senseR > 1.0 ? 1.0 : senseR;
+
+		inputSampleL *= (1.0 - senseL);
+		inputSampleR *= (1.0 - senseR);
+
+		inputSampleL += (smoothL * senseL);
+		inputSampleR += (smoothR * senseR);
+
+		senseL *= 0.5;
+		senseR *= 0.5;
+
+		accumulatorSample = (ataLastOutL * senseL) + (inputSampleL * (1.0 - senseL));
 		ataLastOutL = inputSampleL;
 		inputSampleL = accumulatorSample;
-		
-		accumulatorSample = (ataLastOutR*senseR)+(inputSampleR*(1.0-senseR));
+
+		accumulatorSample = (ataLastOutR * senseR) + (inputSampleR * (1.0 - senseR));
 		ataLastOutR = inputSampleR;
-		inputSampleR = accumulatorSample;		
-		
-		if (wet !=1.0) {
-			inputSampleL = (inputSampleL * wet) + (drySampleL * (1.0-wet));
-			inputSampleR = (inputSampleR * wet) + (drySampleR * (1.0-wet));
+		inputSampleR = accumulatorSample;
+
+		if (wet != 1.0)
+		{
+			const double om_wet = 1.0 - wet;
+			inputSampleL = (inputSampleL * wet) + (drySampleL * om_wet);
+			inputSampleR = (inputSampleR * wet) + (drySampleR * om_wet);
 		}
-		
-		//begin 64 bit stereo floating point dither
-		//int expon; frexp((double)inputSampleL, &expon);
+
 		fpdL ^= fpdL << 13; fpdL ^= fpdL >> 17; fpdL ^= fpdL << 5;
-		//inputSampleL += ((double(fpdL)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
-		//frexp((double)inputSampleR, &expon);
 		fpdR ^= fpdR << 13; fpdR ^= fpdR >> 17; fpdR ^= fpdR << 5;
-		//inputSampleR += ((double(fpdR)-uint32_t(0x7fffffff)) * 1.1e-44l * pow(2,expon+62));
-		//end 64 bit stereo floating point dither
-		
+
 		*out1 = inputSampleL;
 		*out2 = inputSampleR;
 
@@ -220,5 +230,5 @@ void Acceleration::processDoubleReplacing(double **inputs, double **outputs, Vst
 		*in2++;
 		*out1++;
 		*out2++;
-    }
+	}
 }
